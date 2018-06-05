93db9283116b8efb968772c044138e49d97d4401
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2018-06-04 19:32:36 -0700

    Adds Ant Design for React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 class App extends Component {
   render() {
@@ -10,6 +11,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
