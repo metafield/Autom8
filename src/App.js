8affbd6b3de9218e95e67bf1c2753fe74472b52b
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2018-06-05 17:27:03 -0700

    adds basic react component to display Devices

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Device from "./Components/Device";
+import { Switch } from 'antd';
+
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 class App extends Component {
+  onChange(checked) {
+    console.log(`switch to ${checked}`);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Device />
       </div>
     );
   }
---
+++ src/Components/Device.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Icon } from 'antd';
+
+class Device extends React.Component {
+
+  render() {
+    return (
+      <div className="Device">
+        <h1> A Device </h1>
+    <Switch checkedChildren={<Icon type="check" />} unCheckedChildren={<Icon type="cross" />} />
+        <hr/>
+      </div>
+    );
+  }
+}
+
+export default Device;
