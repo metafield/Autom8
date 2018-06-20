f81b793509cb40ccc0395db8d0b36699cb7fb155
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2018-06-19 20:53:44 -0700

    animates nodes like switches when the data changes inside the database

--- src/Components/Devices/Device.js
+++ src/Components/Devices/Device.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDOM from "react-dom";
 import { Switch, Icon } from 'antd';
 
 
-const Device = (props) => {
-  const switchPosition = props.switchPosition ? true : false;
-  console.log(switchPosition);
-  
-  return <div className="Device">
-      <h1> {props.name} </h1>
-      <Switch checkedChildren={<Icon type="check" />} unCheckedChildren={<Icon type="cross" />} defaultChecked={switchPosition} />
-      <hr />
-    </div>;
+class Device extends Component {
+
+  constructor(props) {
+    super(props); 
+  }
+
+  render() {
+    return <div className="Device">
+        <h1> {this.props.name} </h1>
+      <Switch checkedChildren={<Icon type="check" />} unCheckedChildren={<Icon type="cross" />} defaultChecked={this.props.switchPosition ? true : false} />
+        <hr />
+      </div>;
+  }
+
+  componentDidMount() {
+    this.switch = ReactDOM.findDOMNode(this).querySelector(".ant-switch");
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // console.log('prev: ', prevProps, 'prevState ', prevState, 'snap ', snapshot);
+    console.log("prev switch was: ", prevProps.switchPosition);
+    console.log('current switch is: ', this.props);
+    
+    if (prevProps.switchPosition != this.props.switchPosition) {
+      this.switch.click();
+    }
+  }
 }
 
 export default Device;
