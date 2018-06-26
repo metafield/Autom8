57d2a76d0f8e3b288796634e2b4ddbf806eafa42
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2018-06-26 12:05:25 -0700

    adds firebase functions

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.onDataAdded = functions.database.ref('/users/admin/switches').onWrite(event => {
+  const data = event.data.val();
+  console.log(data);
+  
+});
