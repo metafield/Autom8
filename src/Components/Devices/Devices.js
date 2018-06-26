add03d5a80e24d955c5ad69c33ebbf69503b6515
Anthony Sylvester <Anthony.Sylvester99@gmail.com>
2018-06-25 18:24:13 -0700

    adds MediaQuery for React

--- src/Components/Devices/Devices.js
+++ src/Components/Devices/Devices.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Device from "./Device";
+import MediaQuery from 'react-responsive'
 import { fireBaseConnect } from "../../Context/FireBase";
 import { Table } from "antd";
 
@@ -63,9 +64,37 @@ class Devices extends Component {
         }
       ];
 
+      const Example = () => (
+        <div>
+          <div>Device Test!</div>
+          <MediaQuery query="(min-device-width: 1224px)">
+            <div>You are a desktop or laptop</div>
+            <MediaQuery query="(min-device-width: 1824px)">
+              <div>You also have a huge screen</div>
+            </MediaQuery>
+            <MediaQuery query="(max-width: 1224px)">
+              <div>You are sized like a tablet or mobile phone though</div>
+            </MediaQuery>
+          </MediaQuery>
+          <MediaQuery query="(max-device-width: 1224px)">
+            <div>You are a tablet or mobile phone</div>
+          </MediaQuery>
+          <MediaQuery query="(orientation: portrait)">
+            <div>You are portrait</div>
+          </MediaQuery>
+          <MediaQuery query="(orientation: landscape)">
+            <div>You are landscape</div>
+          </MediaQuery>
+          <MediaQuery query="(min-resolution: 2dppx)">
+            <div>You are retina</div>
+          </MediaQuery>
+        </div>
+      );
+
       return (
         <div className="Devices">
           <React.Fragment>
+            <p>{Example()}</p>
             <Table columns={columns} dataSource={this.data} />;
           </React.Fragment>
         </div>
